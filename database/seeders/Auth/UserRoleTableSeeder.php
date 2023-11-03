6616eafa395f0033f9aeb1a1e8511d8981954b63
mikaeldiok <mikaeldiok@gmail.com>
2023-11-03 11:10:55 +0700

    hotfix

--- database/seeders/Auth/UserRoleTableSeeder.php
+++ database/seeders/Auth/UserRoleTableSeeder.php
@@ -23,7 +23,6 @@ class UserRoleTableSeeder extends Seeder
         User::findOrFail(1)->assignRole('super admin');
         User::findOrFail(2)->assignRole('administrator');
         User::findOrFail(3)->assignRole('kepala');
-        User::findOrFail(4)->assignRole('executive');
         User::findOrFail(5)->assignRole('user');
 
         Schema::enableForeignKeyConstraints();
